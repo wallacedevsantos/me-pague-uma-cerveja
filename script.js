5ba90695eb5ecc61094a536fe37b9e074f57987d
wallacedevsantos <wallacedev.oliveirasantos@gmail.com>
2022-10-18 10:52:47 -0300

    Projeto de card com função 'ler mais' e card de cadastro feito com html, css e javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+var button = document.getElementById('leia-me');
+
+button.addEventListener('click', function() {
+    var card = document.querySelector('.card');
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        return button.textContent = 'Ler menos';
+    }
+
+    button.textContent = 'Ler mais';
+    
+});
